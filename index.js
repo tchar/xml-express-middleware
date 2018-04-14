251b04bcd4da4412b865f08c0ead402031cb6287
tchar <tilemachos.charalampous@gmail.com>
2018-04-14 16:59:05 +0300

    Comments in index.js

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+/**
+ * Index module
+ * @module index
+ */
 const xml = require('./lib/xml');
 const send = require('./lib/send');
 
+/**
+ * Export send as default
+ * Export send as send
+ * Export xml as xml
+ */
 module.exports = send;
 module.exports.send = send;
 module.exports.xml = xml;
