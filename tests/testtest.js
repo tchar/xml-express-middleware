4beeb46ce405d443cf1dc898edc849bfb6f49d93
Tilemachos Charalampous <tilemachos.charalampous@gmail.com>
2018-04-17 12:39:30 +0300

    Typescript

---
+++ tests/testtest.js
@@ -0,0 +1,11 @@
+// const app = require('express')();
+// const m = require('../lib/dist/index');
+
+// app.use(m());
+
+// app.get('/b', function(req, res, next){
+//     res.send({a: 'b'});
+// });
+
+
+// app.listen(8095);
